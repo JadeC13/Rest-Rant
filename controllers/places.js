faa94776d969d73e1e7d50fa920c394c877998a0
Jade Chalco <kawaiizom13@gmail.com>
2024-05-14 20:02:32 -0400

    added comments

--- controllers/places.js
+++ controllers/places.js
@@ -33,14 +33,15 @@ router.post('/', (req, res) => {
     })
 })
 
-
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
+    .populate('comments')
     .then(place => {
+        console.log(place.comments)
         res.render('places/show', { place })
     })
     .catch(err => {
